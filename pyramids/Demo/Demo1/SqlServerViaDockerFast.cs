16cbe46da76b9c66218161a9bcb34d117005c63c
Jason Holloway <miceeatnicerice@hotmail.com>
2019-07-22 14:23:06 +0100

    pyramid demo proj

---
+++ pyramids/Demo/Demo1/SqlServerViaDockerFast.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace Sorted.Tracking.Whippet.Test.SqlServerViaDocker
+{
+    public class SqlServerViaDockerFast
+    {
+        private static Lazy<DockerDeduction> _lzDeduced;
+        private static Lazy<DockerClient> _lzDocker;
+        
+        private string _contId;
+        private Uri _contUri;
+
+        public string ConnectionString { get; private set; }
+        
+        static SqlServerViaDockerFast()
+        {
+            _lzDeduced = new Lazy<DockerDeduction>(DockerDeducer.Deduce);
+            _lzDocker = new Lazy<DockerClient>(
+                () => new DockerClientConfiguration(_lzDeduced.Value.DockerApi)
+                        .CreateClient());
+        }
+        
+        public async Task Start(CancellationToken cancel)
+        {
+            var docker = _lzDocker.Value;
+            
+            _contId = await RunContainer();
+            _contUri = await GetContainerUri();
+            
+            ConnectionString =
+                $"Server={_contUri.Host},{_contUri.Port};Database=electio_tracking;User Id=sa;Password=Wibble123!;MultipleActiveResultSets=True";
+            
+            async Task<string> RunContainer()
+            {
+                var contParams = new CreateContainerParameters
+                {
+                    Image = "sqlservr-tracking",
+                    HostConfig = new HostConfig
+                    {
+                        AutoRemove = true,
+                        Privileged = true,
+                        Tmpfs = new Dictionary<string, string>
+                            { ["/run"] = "rw" },
+                        PortBindings = new Dictionary<string, IList<PortBinding>>
+                            { ["1433/tcp"] = new[] { new PortBinding() } }
+                    },
+                    ExposedPorts =new Dictionary<string, EmptyStruct>
+                        { ["1433/tcp"] = default(EmptyStruct) },
+                    Env = new[] { "STOP_AFTER=1m", "RUN_FAST=1" },
+                };
+                
+                var cont = await docker.Containers
+                    .CreateContainerAsync(contParams, cancel);
+                
+                await docker.Containers
+                    .StartContainerAsync(cont.ID, new ContainerStartParameters(), cancel);
+                
+                return cont.ID;
+            }
+            
+            async Task<Uri> GetContainerUri()
+            {
+                var info = await docker.Containers.InspectContainerAsync(_contId, cancel);
+                var net = info.NetworkSettings;
+                
+                var exposedHost = _lzDeduced.Value.ExposedHost;
+                
+                return new Uri($"tcp://{exposedHost}:{net.Ports["1433/tcp"].First().HostPort}");
+            }
+        }
+        
+        public void Close()
+        {
+            if (_contId != null && _lzDocker.IsValueCreated)
+            {
+                var docker = _lzDocker.Value;
+                docker.Containers
+                    .KillContainerAsync(_contId, new ContainerKillParameters());
+            }
+        }
+    }
+}
---
+++ pyramids/Demo/Sorted.Tracking.Whippet.Test.SqlServerViaDocker/buildSqlServrTracking.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+main() {
+	(buildBase sqlservr-base)
+	(buildDerived sqlservr-tracking)
+	echo "Built super fast sqlservr-tracking image"
+}
+
+buildBase() {
+	image=$1
+	cd "$image"
+	docker build -t "$image" .
+}
+
+buildDerived() {
+	image=$1
+	cd "$image"
+
+	old=$(docker image ls -q "$image" || true)
+
+	temp="$image-tmp-$(uuidgen)"
+	docker build -t "$temp" .
+
+	cont=$(docker run -d \
+            --network=host \
+            --privileged \
+            --security-opt seccomp=unconfined \
+            --tmpfs=/run \
+            "$temp" \
+            ./startAndCheckpoint.sh) 
+	docker logs -f "$cont" &
+	docker wait "$cont"
+	
+	docker commit \
+		-c 'CMD ["./start.sh"]' \
+		"$cont" \
+		"$image" 
+
+	docker rm -f "$cont"
+	docker image rm -f "$temp" "$old"
+	wait
+}
+
+main
