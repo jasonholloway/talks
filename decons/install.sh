00279ad8b560ea592f6d11681b77794cd071be25
Jason Holloway <jason.holloway@sorted.com>
2021-08-14 20:54:23 +0100

    decons updates

---
+++ decons/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+(wget https://github.com/hakimel/reveal.js/archive/master.zip &&
+     unzip master.zip &&
+     ln -s reveal.js-master reveal.js)
+
+(cd reveal.js/dist/theme &&
+     wget https://revealjs-themes.dzello.com/css/theme/sunblind.css)
+
+(cd reveal.js/plugin &&
+     git clone git@github.com:burnpiro/reveal-pointer.git)
+
+(cd reveal.js/plugin &&
+     git clone git@github.com:burnpiro/reveal-drawer.git)
+
+(git clone git@github.com:csev/dazzleSketch.git)
